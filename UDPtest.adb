9e0eb8ecf78b74e87ef0b18fa2b530ce80bcdb0c
crosssoniky <vindicare28@yahoo.co.jp>
2016-02-25 22:49:28 +0900

    60061→60010へUDP

--- UDPtest.adb
+++ UDPtest.adb
@@ -7,25 +7,18 @@ procedure UDPtest is
    Client : Socket_Type;
    Address: Sock_Addr_Type;
    Channel: Stream_Access;
-   Host_Entry:Host_Entry_Type:=Get_Host_By_Name("sonic.sega.jp");
-   Send   : String:=ASCII.CR & ASCII.LF;
-   Offset : Ada.Streams.Stream_Element_Count;
-   Data   : Ada.Streams.Stream_Element_Array(1 .. 256);
 begin
    GNAT.Sockets.Initialize;
    Create_Socket(Client,Family_Inet,Socket_Datagram);
    Address.Addr:=Inet_Addr("127.0.0.1");
+   Address.Port:=60061;
+   Bind_Socket(Client,Address);
    Address.Port:=60060;
+   --Connect_Socket(Client,Address);
+   Channel:=Stream(Client,Address);
 
-   Connect_Socket(Client,Address);
-   Channel:=Stream(Client);
-
-   String'Write(Channel, "GET / HTTP/1.1" & Send & "Host:sonic.sega.jp"&Send&Send);	--HTTP_Send
-   loop
-   Ada.Streams.Read(Channel.all,Data,Offset);
-      exit when  Offset=0;
-      for I in 1 .. Offset loop
-         Put(Character'Val(Data(I)));
-      end loop;
-   end loop;
+   String'Write(Channel, "I'm hungry.");	--HTTP_Send
+   Free(Channel);
+   Close_Socket(Client);
+   Finalize;
 end UDPtest;
